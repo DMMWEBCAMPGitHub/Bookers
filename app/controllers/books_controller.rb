7547f752587eb70e2ecaf7fe9aa4914b9dfc7bc6
DMMWEBCAMPGitHub <130573254+DMMWEBCAMPGitHub@users.noreply.github.com>
2023-05-16 07:09:45 +0000

    add3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,41 @@
 class BooksController < ApplicationController
-  def new
+
+def create
+    @book = Book.new(book_params)
+    if@book.save
+    redirect_to book_path(@book.id)
+    else
+    render :new
+    end
   end
 
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+   def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+
+  private
+  def list_params
+    params.require(:book).permit(:title, :body, :image)
   end
 end
+
